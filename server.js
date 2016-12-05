6583826e7367da0ac2dbcb999517eb8d7b4199eb
Nadolskyi <nadolskyy@i.ua>
2016-12-05 17:17:42 +0000

    serverOk

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 var http = require('http');
-var hostname = process.argv[5];
-var port =process.argv[3];
-if(!port||!hostname){
-  hostname = '127.3.3.3';
-     port = 3000; 
-}
-
-
-var server = http.createServer((req, res) => {
-res.statusCode=200;
-  res.setHeader('Content-Type','text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World from Cloud9\n');
+}).listen(process.env.PORT);
