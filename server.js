5c7e7ac1fd26fb227a5bf1d1574f92d6c5676fdc
Nadolskyi <nadolskyy@i.ua>
2016-12-06 06:41:16 +0000

    serverExpress

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Cloud9\n');
+var express = require('express');
+var app = express();
+
+app.get('/',function(req, res) {
+   res.write('hhhexpress');
 }).listen(process.env.PORT);
+console.log('Server has started!');
