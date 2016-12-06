d3217b22ca44d521c0273a5b2b748786cc8e1e40
Nadolskyi <nadolskyy@i.ua>
2016-12-06 07:33:44 +0000

    express

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req, res) {
-   res.write('hhhexpress');
-})
-app.listen(process.env.PORT);
+   res.send('hhhexpress');
+}).listen(process.env.PORT);
 console.log('Server has started!');
