8a23714827de106e5430fffd821676d4c762ee2e
Nadolskyi <nadolskyy@i.ua>
2016-12-06 09:01:36 +0000

    rout

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req, res) {
-   res.send('hhhexpress');
+app.get('/', function (req, res, next) {
+
+   res.sendfile('./index.html', function(err) {
+    if (err) {
+        next(err);
+    } else {
+        console.log('transferred %s', path);
+    }
+});
+
 }).listen(process.env.PORT);
 console.log('Server has started!');
