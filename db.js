098b91e468ff04365c70b3c5975499e9ceb924cf
Nadolskyi <nadolskyy@i.ua>
2016-12-07 16:26:17 +0000

    jade

--- db.js
+++ db.js
@@ -12,4 +12,19 @@
  "id": "3",
  "firstName": "Andrew",
  "lastName": "Nadolskyi"
+},
+{
+ "id": "1",
+ "firstName": "Vlad",
+ "lastName": "Nadolskyi"
+},
+{
+ "id": "2",
+ "firstName": "Maria",
+ "lastName": "Nadolska"
+},
+{
+ "id": "3",
+ "firstName": "Andrew",
+ "lastName": "Nadolskyi"
 }]
