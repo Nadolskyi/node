585d49a991fc110ab1350ecb02668db337cd2117
Nadolskyi <nadolskyy@i.ua>
2016-12-07 20:58:10 +0000

    mongo

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-var express = require('express');
-var app = express();
-var fs = require ('fs');
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
+ var db;
 
-app.set('view engine', 'jade');
+MongoClient.connect('mongodb://Nadolskyi:123qaz@ds127988.mlab.com:27988/mongo-crud', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(process.env.PORT, () => {
+    console.log('server ruining');
+  });
+ });
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-
-fs.readFile ('db.js', function(err, content){
-    if (err) { console.log('eror');
-        } else {
-            var par = JSON.parse(content);
-            res.render('jade1', {myData: par});
-        }
-});
-}).listen(process.env.PORT);
+ app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+    });
+ });
+ 
+  app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
+})
